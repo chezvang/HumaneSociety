42a986d0f0e6bbc1d67ae51b0aaaf6b36a7e103f
Chez <chez.vang@gmail.com>
2017-12-21 07:31:52 -0600

    finished adopter new user menu

--- HumaneSociety/HumaneSociety/Adopter.cs
+++ HumaneSociety/HumaneSociety/Adopter.cs
@@ -8,11 +8,54 @@ namespace HumaneSociety
 {
     class Adopter
     {
+        UserInterface ui = new UserInterface();
 
-        public void AdopterMainMenu()
+        public void AdopterNewUserPrompt()
         {
             Console.WriteLine("Are you a new user?"); // y/n?
-            Console.ReadLine();
+            string option = Console.ReadLine();
+            AdopterNewUserOption(option);
+        }
+
+        public void AdopterNewUserOption(string option)
+        {
+            switch(option)
+            {
+                case "y":
+                    AdopterNewProfile();
+                    break;
+                case "n":
+                    AdopterMainMenu();
+                    break;
+                default:
+                    ui.IncorrectInput();
+                    break;
+            }
+        }
+
+        public void AdopterNewProfile()
+        {
+            //These push to the SQL Database for user
+            string userProfile;
+            string userName;
+            string userAge;
+            string userGender;
+            string userEmail;
+            string userPhone;
+
+            Console.WriteLine("Let's set up your new profile!");
+            userName = ui.GetAnimalName();
+            userAge = ui.GetAge();
+            userGender = ui.GetGender();
+            userEmail = ui.GetEmail();
+            userPhone = ui.GetPhone();
+            userProfile = ui.GetProfile();
+            //Confirmation method for inputted data "is this information correct?"
+        }
+
+        public void AdopterMainMenu() //adopter search menu
+        {
+            Console.WriteLine("");
         }
     }
 }
